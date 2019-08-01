d8c7175b09294eaf2a36093f98245c69ccefcc8f
zhangwenbin <17637500867@163.com>
2019-08-01 14:46:50 +0800

    nacos作为配置中心

--- alibaba/nacos-provider/src/main/java/cn/ali/nacos/controller/ProviderContreoller.java
+++ alibaba/nacos-provider/src/main/java/cn/ali/nacos/controller/ProviderContreoller.java
@@ -2,6 +2,7 @@ package cn.ali.nacos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
+@RefreshScope
 public class ProviderContreoller {
 
     @Value("${spring.application.name}")
     private String servicename;
 
+    @Value("${user.name}")
+    private String name;
+
     @GetMapping("/hi")
     public String hi(){
         return "hi: "+servicename;
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "hi: "+name;
     }
 }
